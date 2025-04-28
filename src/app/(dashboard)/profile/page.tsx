856953a198bc1a1037516e3226f930e0a3d01c8c
Dev Rudra <75237456+devrudraa@users.noreply.github.com>
2025-04-28 23:19:36 +0530

    Made the profile responsive

--- src/app/(dashboard)/profile/page.tsx
+++ src/app/(dashboard)/profile/page.tsx
@@ -8,8 +8,8 @@ export default function Page() {
   return (
     <section>
       <section className="p-4 rounded-lg border border-primary/30 bg-primary/5 bg-[url('/images/support-banner-bg.png')] object-fill space-y-5">
-        <div className="flex  items-center justify-between">
-          <div className="flex gap-3">
+        <div className="flex md:items-center md:justify-between flex-col md:flex-row gap-4">
+          <div className="flex flex-col xs:flex-row gap-3">
             <Image
               src={"/images/pfp.jpg"}
               width={75}
@@ -20,16 +20,16 @@ export default function Page() {
             <div className="-space-y-1">
               <h1 className="text-2xl font-bold">John Smith</h1>
               <h2 className="font-medium text-primary">HSung15</h2>
-              <h3 className="text-muted-foreground">
+              <h3 className="text-muted-foreground break-words">
                 emailadress123@gmail.com
               </h3>
             </div>
           </div>
           <Link href={"/profile/edit"}>
-            <Button>Edit Profile</Button>
+            <Button className="w-full md:w-auto">Edit Profile</Button>
           </Link>
         </div>
-        <div className="flex gap-2">
+        <div className="flex gap-2 flex-col sm:flex-row">
           <ChangePasswordDialog />
           <DeleteAccountDialog />
         </div>
