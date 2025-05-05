293ccfa62f7168725ef3a896dc4836b0afa326bb
Dev Rudra <75237456+devrudraa@users.noreply.github.com>
2025-05-05 23:42:38 +0530

    Fixed ImageCard prop error

--- src/app/(dashboard)/(pages)/image-generation/page.tsx
+++ src/app/(dashboard)/(pages)/image-generation/page.tsx
@@ -26,10 +26,22 @@ export default function Page() {
       <div className="space-y-3">
         <ImageGridHeader />
         <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
+          <ImageCard
+            imageSrc="/images/image-card.png"
+            imgAlt="image-card.png"
+          />
+          <ImageCard
+            imageSrc="/images/image-card.png"
+            imgAlt="image-card.png"
+          />
+          <ImageCard
+            imageSrc="/images/image-card.png"
+            imgAlt="image-card.png"
+          />
+          <ImageCard
+            imageSrc="/images/image-card.png"
+            imgAlt="image-card.png"
+          />
         </div>
       </div>
     </section>
--- src/components/pages/dashboard/image-card-grid.tsx
+++ src/components/pages/dashboard/image-card-grid.tsx
@@ -7,10 +7,22 @@ export default function ImageCardGrid() {
       <div className="space-y-3">
         <ImageGridHeader />
         <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
+          <ImageCard
+            imageSrc="/images/image-card.png"
+            imgAlt="image-card.png"
+          />
+          <ImageCard
+            imageSrc="/images/image-card.png"
+            imgAlt="image-card.png"
+          />
+          <ImageCard
+            imageSrc="/images/image-card.png"
+            imgAlt="image-card.png"
+          />
+          <ImageCard
+            imageSrc="/images/image-card.png"
+            imgAlt="image-card.png"
+          />
         </div>
       </div>
     </section>
