754817e7747637592e25aec34e2a888181531ba0
DeepMakhasana <deepmakhasana1013@gmail.com>
2025-07-17 13:42:35 +0530

    plan content change

--- src/components/pages/subscription/subscription-pricing.tsx
+++ src/components/pages/subscription/subscription-pricing.tsx
@@ -41,14 +41,11 @@ export default function SubscriptionPricing() {
 
   return (
     <>
-      <SubscriptionHeader
-        billingPeriod={billingPeriod}
-        setPeriod={setBillingPeriod}
-      />
+      <SubscriptionHeader billingPeriod={billingPeriod} setPeriod={setBillingPeriod} />
 
       {/* Pricing Cards */}
       {/* <div className="flex items-center flex-wrap justify-center gap-6"> */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {pricingPlansDetails.map((card) => (
           <PricingCard
             {...card}
@@ -56,8 +53,7 @@ export default function SubscriptionPricing() {
             billingPeriod={billingPeriod}
             isLoading={status != "authenticated"}
             isCurrentPlan={
-              data?.user.subscription.planType === card.id &&
-              data?.user.subscription.interval === billingPeriod
+              data?.user.subscription.planType === card.id && data?.user.subscription.interval === billingPeriod
             }
           />
         ))}
@@ -66,32 +62,35 @@ export default function SubscriptionPricing() {
   );
 }
 
-const pricingPlansDetails: Omit<
-  PricingCardProps,
-  "billingPeriod" | "isCurrentPlan" | "isLoading"
->[] = [
+const pricingPlansDetails: Omit<PricingCardProps, "billingPeriod" | "isCurrentPlan" | "isLoading">[] = [
   {
     id: "STARTER",
     title: "Starter",
+    description: "Ideal for Independent Artists",
     price: 27,
     price_annual: 219,
     features: [
-      "5 image generations per month",
-      "Standard resolution",
-      "Basic support",
+      "Generate 5 covers per month",
+      "Standard-resolution outputs",
+      "Access to basic customization tools",
+      "Full ownership of downloaded designs",
+      "Mobile access for convenience",
+      "Dedicated email support",
     ],
     color: "secondary",
     icon: <StickyNoteIcon color="white" />,
   },
   {
     id: "NEXT_LEVEL",
-    title: "Next level",
+    title: "Next Level",
+    description: "Designed for Serious Artists Ready to Grow",
     price: 67,
     price_annual: 539,
     features: [
-      "Unlimited image generations",
-      "High resolution output",
-      "Advanced customization options",
+      "Unlimited AI-powered cover designs",
+      "Custom text tools for enhanced branding",
+      "High-resolution outputs for pro-quality results",
+      "Priority email support (24-hour response time)",
     ],
     color: "blue",
     icon: <Layers2Icon color="white" />,
@@ -99,12 +98,15 @@ const pricingPlansDetails: Omit<
   {
     id: "PRO_STUDIO",
     title: "Pro Studio",
+    description: "Designed for Businesses",
     price: 119,
     price_annual: 971,
     features: [
-      "Unlimited HD Image Generations",
-      "Priority Support & Account Manager",
-      "Team Collaboration & User Management",
+      "Add up to 5 team members for collaborative access",
+      "Centralized dashboard to manage projects, users, and downloads",
+      "Priority queue for cover generation (even faster processing)",
+      "Team-based folders for organized content management",
+      "Early access to beta features & experimental tools",
     ],
     color: "primary",
     icon: <Layers3Icon color="white" />,
