e4b169f0b6c512d3dc3ab75172aabd77eb492a6e
DeepMakhasana <deepmakhasana1013@gmail.com>
2025-07-25 08:23:35 +0530

    Generate Now and Remix image button center in mobile

--- src/app/(dashboard)/(pages)/remix-image/page.tsx
+++ src/app/(dashboard)/(pages)/remix-image/page.tsx
@@ -6,16 +6,14 @@ import Link from "next/link";
 export default function Page() {
   return (
     <section className="space-y-5">
-      <div className="flex flex-col gap-5 py-10 text-center sm:text-left justify-between px-3 sm:py-5 bg-[url('/images/cta-2.jpg')] bg-no-repeat object-fill bg-cover rounded-xl">
+      <div className="flex flex-col gap-5 py-7 text-center sm:text-left justify-between px-3 sm:py-5 bg-[url('/images/cta-2.jpg')] bg-no-repeat object-fill bg-cover rounded-xl">
         <div className="text-white">
-          <h1 className="font-bold text-xl">Remix Wizard</h1>
-          <p className="text-sm">
-            Hit &quot;remix&quot; and craft your custom album cover in seconds
-          </p>
+          <h1 className="font-bold text-xl mb-1 md:mb-0.5">Remix Wizard</h1>
+          <p className="text-xs xs:text-sm">Hit &quot;remix&quot; and craft your custom album cover in seconds</p>
         </div>
-        <div className="w-fit">
-          <Link href={"/remix-image/remix"} className="w-fit">
-            <Button className="w-full sm:w-auto" variant={"secondary"}>
+        <div>
+          <Link href={"/remix-image/remix"}>
+            <Button className="sm:w-auto" variant={"secondary"}>
               Remix Image <GIcon>wand_stars</GIcon>
             </Button>
           </Link>
