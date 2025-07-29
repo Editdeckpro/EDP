e80a5c9f384a42524c82001a55bf672954236296
DeepMakhasana <deepmakhasana1013@gmail.com>
2025-07-29 17:04:30 +0530

    subscription status message add in manage subscription

--- src/components/pages/subscription/subscription-header.tsx
+++ src/components/pages/subscription/subscription-header.tsx
@@ -1,23 +1,68 @@
-import React, { Dispatch, FC, SetStateAction } from "react";
+"use client";
+import React, { Dispatch, FC, SetStateAction, useEffect, useState } from "react";
 import { billingPeriod } from ".";
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 import { cn } from "@/lib/utils";
+import { GetAxiosWithAuth } from "@/lib/axios-instance";
+import { GetCurrentSubscriptionResponse } from "./types";
+import { formatLongDate } from "@/helper/helper-functions";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface SubscriptionHeaderProps {
   setPeriod: Dispatch<SetStateAction<billingPeriod>>;
   billingPeriod: billingPeriod;
 }
-const SubscriptionHeader: FC<SubscriptionHeaderProps> = ({
-  setPeriod,
-  billingPeriod,
-}) => {
+const SubscriptionHeader: FC<SubscriptionHeaderProps> = ({ setPeriod, billingPeriod }) => {
+  const [subscriptionData, setSubscriptionData] = useState<GetCurrentSubscriptionResponse | null>(null);
+  const [expireDate, setExpireDate] = useState<string>("");
+
+  useEffect(() => {
+    switch (subscriptionData?.messageMetadata.type) {
+      case "CANCEL_AT_PERIOD_END":
+        setExpireDate(subscriptionData?.messageMetadata?.endDate);
+        break;
+      case "TRIAL_END":
+        setExpireDate(subscriptionData?.messageMetadata?.trialEndDate);
+        break;
+      case "EXPIRED":
+      case "CANCELED":
+        setExpireDate(subscriptionData?.messageMetadata?.expireDate);
+        break;
+      case "SCHEDULED_DOWNGRADE":
+        setExpireDate(subscriptionData?.messageMetadata?.downgradeDetails?.effectiveDate);
+        break;
+    }
+  }, [subscriptionData]);
+
+  useEffect(() => {
+    const fetchSubscription = async () => {
+      try {
+        const axios = await GetAxiosWithAuth();
+        const res = await axios.get<GetCurrentSubscriptionResponse>(`subscription`);
+        console.log("subscription res", res);
+        setSubscriptionData(res.data);
+      } catch (err) {
+        console.error("Failed to fetch subscription info:", err);
+      }
+    };
+
+    fetchSubscription();
+  }, []);
+
   return (
     <div className="flex md:justify-between items-center md:flex-row flex-col text-center justify-center md:text-left">
       <div>
         <h2 className="text-3xl font-bold mb-2">Choose the Perfect Plan.</h2>
-        <p className="text-[#6a6c7b] mb-6">
-          Select a plan that suits your needs and unlock exclusive features.
-        </p>
+        <p className="text-[#6a6c7b]">Select a plan that suits your needs and unlock exclusive features.</p>
+        {subscriptionData?.messageMetadata?.type ? (
+          <p className="text-primary mb-2 font-medium">
+            {subscriptionData?.messageMetadata?.type === "FREE_PLAN"
+              ? subscriptionData?.messageMetadata?.message
+              : `${subscriptionData?.messageMetadata?.message} ${formatLongDate(expireDate)}`}
+          </p>
+        ) : (
+          <Skeleton className="w-80 h-5 mt-1" />
+        )}
       </div>
       <div className="mb-6">
         <ToggleGroup
@@ -30,9 +75,7 @@ const SubscriptionHeader: FC<SubscriptionHeaderProps> = ({
             value="monthly"
             className={cn(
               "px-6 py-2 !rounded-full cursor-pointer",
-              billingPeriod === "monthly"
-                ? "!bg-primary !text-white"
-                : "text-[#6a6c7b]"
+              billingPeriod === "monthly" ? "!bg-primary !text-white" : "text-[#6a6c7b]"
             )}
           >
             Monthly
@@ -41,9 +84,7 @@ const SubscriptionHeader: FC<SubscriptionHeaderProps> = ({
             value="yearly"
             className={cn(
               "px-6 py-2 !rounded-full cursor-pointer",
-              billingPeriod === "yearly"
-                ? "!bg-primary !text-white"
-                : "text-[#6a6c7b]"
+              billingPeriod === "yearly" ? "!bg-primary !text-white" : "text-[#6a6c7b]"
             )}
           >
             Yearly
@@ -54,4 +95,5 @@ const SubscriptionHeader: FC<SubscriptionHeaderProps> = ({
     </div>
   );
 };
+
 export default SubscriptionHeader;
--- src/components/pages/subscription/types.d.ts
+++ src/components/pages/subscription/types.d.ts
@@ -12,14 +12,45 @@ export type SubscriptionPlan = {
 // Get Available Plans Response
 export type GetPlansResponse = SubscriptionPlan[];
 
+type MessageMetadata =
+  | {
+      type: "FREE_PLAN";
+      message: string;
+    }
+  | {
+      type: "CANCEL_AT_PERIOD_END";
+      message: string;
+      endDate: string;
+    }
+  | {
+      type: "TRIAL_END";
+      message: string;
+      trialEndDate: string;
+    }
+  | {
+      type: "EXPIRED" | "CANCELED";
+      message: string;
+      expireDate: string;
+    }
+  | {
+      type: "SCHEDULED_DOWNGRADE";
+      message: string;
+      downgradeDetails: {
+        fromPlan: PlanType;
+        toPlan: PlanType;
+        effectiveDate: string;
+      };
+    };
+
 // Get Current Subscription Response
 export type GetCurrentSubscriptionResponse = {
   planType: string;
   status: string;
-  interval: string;
-  currentPeriodEnd: string;
-  cancelAtPeriodEnd: boolean;
+  interval?: string;
+  currentPeriodEnd?: string;
+  cancelAtPeriodEnd?: boolean;
   features: string[];
+  messageMetadata: MessageMetadata;
 };
 
 // Create Checkout Session Response
