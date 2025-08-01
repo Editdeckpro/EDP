0d762c2c3141bf003c62309f75887a3c7d28fd40
DeepMakhasana <deepmakhasana1013@gmail.com>
2025-08-01 07:13:07 +0530

    fix/build fail

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,4 @@
 import AuthGuard from "@/auth-guard";
-import { TourProvider } from "@/context/OnboardingTourContext";
 import Providers from "@/lib/providers";
 import React, { FC } from "react";
 
@@ -10,13 +9,11 @@ const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <AuthGuard>
       <Providers>
-        <TourProvider>
-          <main className="relative min-h-screen flex flex-col">
-            <div className="block bg-[#3E4EBA]/30 top-0 left-0 md:bg-[#3E4EBA]/70 absolute md:top-10 md:left-1/12 w-52 h-52 rounded-full blur-3xl -z-10" />
-            <div className="block  bg-[#FF8A3D]/30 bottom-0 left-0 md:bg-[#FF8A3D]/70 absolute md:bottom-0 md:left-0 w-52 h-52 rounded-full blur-3xl -z-10" />
-            <div className="flex-1">{children}</div>
-          </main>
-        </TourProvider>
+        <main className="relative min-h-screen flex flex-col">
+          <div className="block bg-[#3E4EBA]/30 top-0 left-0 md:bg-[#3E4EBA]/70 absolute md:top-10 md:left-1/12 w-52 h-52 rounded-full blur-3xl -z-10" />
+          <div className="block  bg-[#FF8A3D]/30 bottom-0 left-0 md:bg-[#FF8A3D]/70 absolute md:bottom-0 md:left-0 w-52 h-52 rounded-full blur-3xl -z-10" />
+          <div className="flex-1">{children}</div>
+        </main>
       </Providers>
     </AuthGuard>
   );
